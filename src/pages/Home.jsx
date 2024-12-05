9717cc1129a1273f3c7ffeb4a7941d49261e458b
ayushash1 <ayushashtikar95@gmail.com>
2024-12-05 13:50:35 +0530

    vercel error fix attempt 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react'
 import appwriteService from "../appwrite/config";
-import {Container, PostCard} from '../components'
+import { Container } from '../components/Container'
+import { PostCard } from '../components/PostCard'
 
 function Home() {
     const [posts, setPosts] = useState([])
